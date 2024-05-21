5c5481c25a8925d99d669a89662de7e6fbe5a091
Johnmclf <150351096+Johnmclf@users.noreply.github.com>
2024-05-21 16:07:25 +0200

    init app

---
+++ app.py
@@ -0,0 +1,23 @@
+﻿import streamlit as st
+import pandas as pd
+import numpy as np
+import json
+
+#recuperer le tableau grace a un fichier 
+#modifier a partir d'un numero de ligne (exemple ligne 159)
+#modifier a partir d'un sting (exemple lio retourn lion,lionne,...)
+
+#Simplifier la modification 
+#permettre de rajouter des donnees a la fin
+
+
+# Ouvrir le fichier JSON et charger les données
+with open('test.json', 'r') as f:
+    data = json.load(f)
+
+# Convertir les données en DataFrame
+df = pd.DataFrame(data)
+
+print(df)
+
+#normalement on touve la ligne on la modifie et on le renvoie 
