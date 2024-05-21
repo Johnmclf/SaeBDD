fe4584ff10147406f53500e1063651947051d10b
Johnmclf <150351096+Johnmclf@users.noreply.github.com>
2024-05-21 16:33:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,16 @@
-﻿import streamlit as st
+import streamlit as st
 import pandas as pd
 import numpy as np
 import json
 
-message = "vide"
-# Demander à l'utilisateur d'entrer un message
 message = st.text_input('Entrez votre message ici')
 
 if message:
-    # Afficher le message
     st.write('Vous avez écrit : ', message)
 
 
-# Ouvrir le fichier JSON et charger les données
 if message:
     with open(message, 'r') as f:
         data = json.load(f)
-
-
-# Ouvrir le fichier JSON et charger les données
-with open(message, 'r') as f:
-    data = json.load(f)
-
-# Convertir les données en DataFrame
-df = pd.DataFrame(data)
-
-st.write(df)
+        df = pd.DataFrame(data)
+        st.write(df)
