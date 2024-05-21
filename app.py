85dd73227580d6ed2855508344ce198e0b2a4b96
Johnmclf <150351096+Johnmclf@users.noreply.github.com>
2024-05-21 16:21:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,18 @@ import pandas as pd
 import numpy as np
 import json
 
+message = "vide"
 # Demander à l'utilisateur d'entrer un message
 message = st.text_input('Entrez votre message ici')
 
-# Afficher le message
-st.write('Vous avez écrit : ', message)
+if(message != "vide"):
+    # Afficher le message
+    st.write('Vous avez écrit : ', message)
+
+    # Ouvrir le fichier JSON et charger les données
+    with open(message, 'r') as f:
+        data = json.load(f)
+
 
 # Ouvrir le fichier JSON et charger les données
 with open(message, 'r') as f:
